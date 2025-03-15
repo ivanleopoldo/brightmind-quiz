ee5ee739668dfe6afa2b7cd8803c6b93ba138214
ivanleopoldo <ivanleopoldoc@gmail.com>
2025-03-15 18:22:29 +0800

    feat: prod

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,5 @@
 import mongoose from "mongoose";
 
-const { MONGODB_URI } = process.env;
-
-if (!MONGODB_URI) throw new Error("MONGO_URL is not defined.");
-
 let cached = global.mongoose;
 
 if (!cached) {
@@ -13,14 +9,14 @@ if (!cached) {
 export const dbConnect = async () => {
   if (cached.conn) return cached.conn;
 
-  try{
-  cached.conn = await mongoose.connect(MONGODB_URI);
-  }
-  catch (error) {
+  console.log(process.env.MONGODB_URI);
+
+  try {
+    cached.conn = await mongoose.connect(process.env.MONGODB_URI!);
+  } catch (error) {
     console.error("Error connecting to MongoDB:", error);
     throw error;
   }
-
 
   return cached.conn;
 };

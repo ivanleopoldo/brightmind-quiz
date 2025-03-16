856936608c4939b41be06cb2d7ab4553e096a069
ivanleopoldo <ivanleopoldoc@gmail.com>
2025-03-16 10:57:36 +0800

    feat: test

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,29 +1,40 @@
 import mongoose from "mongoose";
 
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local",
+  );
+}
+
 let cached = global.mongoose;
 
 if (!cached) {
-  cached = global.mongoose = { conn: null };
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
-export const dbConnect = async () => {
-  if (cached.conn) return cached.conn;
-
-  try {
-    cached.conn = await mongoose.connect(process.env.MONGODB_URI!, {
-      autoIndex: true,
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      console.log("Db connected");
+      return mongoose;
     });
-    mongoose.set("bufferCommands", false);
-    if (cached.conn) {
-      mongoose.set("debug", true);
-      console.log("Connected to MongoDB");
-    }
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-    throw error;
+  }
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
   }
 
   return cached.conn;
-};
+}
 
 export default dbConnect;
